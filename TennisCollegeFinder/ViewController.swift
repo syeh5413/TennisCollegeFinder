989373bf3cf80d833cac9ec570ccb0922b8c19c3
syeh5413 <syeh5413@stu.d214.org>
2023-04-16 23:18:47 -0500

    added some stuff to make it a double

--- TennisCollegeFinder/ViewController.swift
+++ TennisCollegeFinder/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var ratingTextField: UITextField!
-    var colleges : [String] = []
-
+    var colleges: [String] = []
+    var rating: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let ratingText = ratingTextField.text {
+            rating = Double(ratingText)
+        }
+        
+        print(rating)
     }
+    
+    
 }
 
