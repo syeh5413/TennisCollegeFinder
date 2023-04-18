53c5875297ecec3620262170faf5c0ce4fd4a703
Sam Yeh <syeh5413@stu.d214.org>
2023-04-18 11:15:43 -0500

    made a dictionary of colleges

--- TennisCollegeFinder/ViewController.swift
+++ TennisCollegeFinder/ViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var scoreTextField: UITextField!
-    var colleges: [String] = []
+    var colleges = ["Princeton": 1460...1570,
+                    "University of Miami": 1300...1460,
+                    "Illinois State": 1000...1200]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    
+    @IBAction func whenCollegeButtonPressed(_ sender: Any) {
+//        for college in colleges {
+//            if ((colleges[college.value]?.contains(Int(scoreTextField.text!)!)) != nil) {
+//
+//            }
+//        }
+    }
 }
 
