6700dad4997568d50c9dc7f0ecaba7205ff76a6f
Sam Yeh <syeh5413@stu.d214.org>
2023-04-20 10:30:21 -0500

    finished function

--- TennisCollegeFinder/ChoiceViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ChoiceViewController.swift
-//  TennisCollegeFinder
-//
-//  Created by Sam Yeh on 4/6/23.
-//
-
-import Foundation
--- TennisCollegeFinder/ViewController.swift
+++ TennisCollegeFinder/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var scoreTextField: UITextField!
+    @IBOutlet weak var sentenceLabel: UILabel!
+    @IBOutlet weak var collegeImage: UIImageView!
     var collegeScores = ["Princeton": 1460...1570,
                          "University of Miami": 1300...1460,
                          "Illinois State": 1000...1200]
@@ -22,13 +24,13 @@ class ViewController: UIViewController {
         if let scoreText = scoreTextField.text, let score = Double(scoreText) {
             for (collegeName, scoreRange) in collegeScores {
                 if scoreRange.contains(Int(score)) {
-                    print("You qualify for \(collegeName)!")
+                    sentenceLabel.text = "Congrats! Based on your score of \(score), you qualify for \(collegeName)."
                     return
                 }
             }
-            print("Sorry, you don't qualify for any colleges.")
+            sentenceLabel.text = "Sorry, you don't qualify for any colleges."
         } else {
-            print("Invalid score.")
+            sentenceLabel.text = "You entered an invalid score."
         }
     }
 }
